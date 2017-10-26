b23c1b361e661c83d457c14bfcae49e71f96b372
criscrack <cristian_gonzalez_alonso@hotmail.com>
2017-10-26 16:37:24 +0000

    Comentarios explicativos añadidos

--- lib/alimentos.rb
+++ lib/alimentos.rb
@@ -17,27 +17,33 @@ def initialize(alimento,proteinas,glucidos,lipidos)
     
 end
 
+#Método para obtener el nombre del alimento
 def get_ali(i)
     @alimento[i]
 end
 
+#Método para obtener las proteinas del alimento
 def get_proteinas(i)
     @proteinas[i]
 end
 
+#Método para obtener los glucidos del alimento
 def get_glucidos(i)
     @glucidos[i]
 end
 
+#Método para obtener los lipidos del alimento
 def get_lipidos(i)
     @lipidos[i]
 end
 
+#Método para obtener el valor energético del alimento
 def get_calorias(i)
     @calorias = [0.0]
     @calorias [i] = ((@proteinas[i]*4)+(@glucidos[i]*4)+(@lipidos[i]*9))
 end
 
+#Método para obtener el formateado del alimento
 def get_formateado(i)
     get_calorias(i)
     "#{@alimento[i]}" " = " "#{@proteinas[i]}" " gr de proteinas + " "#{@glucidos[i]}" " gr de glucidos + " "#{@lipidos[i]}" " gr de lipidos | " "#{@calorias[i]}" " Kcal en total"
